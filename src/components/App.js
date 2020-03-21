4a40995309eebc85368cb3add83860399b973888
manaki <mana23happy@hotmail.co.jp>
2020-03-21 17:30:10 +0900

    :+1:親から値を渡す

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,50 @@
-import React from 'react';
-import Header from './Header';
-import ToDo from './ToDo';
+import React from "react";
+import Header from "./Header";
+import ToDo from "./ToDo";
 import stylesheet from "./stylesheet.css";
 
-
 class App extends React.Component {
-  componentWillMount(){
+  componentWillMount() {
     let user_array = [
       {
-        name: 'Ryuto_Shirakawa',
+        name: "Ryuto_Shirakawa",
         like_point: 100,
         liked_point: 0,
-        img: '',
+        img: ""
       },
-        {
-          name: 'Manaki_Ikeda',
-          like_point: 100,
-          liked_point: 0,
-          img: '',
-        },
-        {
-          name: 'Daichi_Nakashizu',
-          like_point: 100,
-          liked_point: 0,
-          img: '',
-        },
-      ];
-      let setjson = JSON.stringify(user_array);
-      /*global localStorage*/
-      localStorage.setItem('user_info',setjson);
+      {
+        name: "Manaki_Ikeda",
+        like_point: 100,
+        liked_point: 0,
+        img: ""
+      },
+      {
+        name: "Daichi_Nakashizu",
+        like_point: 100,
+        liked_point: 0,
+        img: ""
+      }
+    ];
+    let setjson = JSON.stringify(user_array);
+    /*global localStorage*/
+    localStorage.setItem("user_info", setjson);
   }
-  
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user_info: localStorage.getItem("user_info")
+    };
+  }
+
   render() {
-    return(
-      <div className='App'>
-        <Header />
+    return (
+      <div className="App">
+        <Header user_info={this.state.user_info} />
         <ToDo />
       </div>
-      );
+    );
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,40 @@
-import React from 'react';
+import React from "react";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { user_info: [] };
+  }
   render() {
-    return(
+    return (
       <div className="l-header">
         <div className="l-header__wrapper">
-            <div className="l-header__box">
-                <img className="l-header__img" 
-                  src={"https://pbs.twimg.com/profile_images/1197045010786480128/8GC1D9og_400x400.jpg"} alt={''} 
-                />
-                <select name="" id="" className="l-header__select">
-                </select>
+          <div className="l-header__box">
+            <img
+              className="l-header__img"
+              src={
+                "https://pbs.twimg.com/profile_images/1197045010786480128/8GC1D9og_400x400.jpg"
+              }
+              alt={""}
+            />
+            <select name="" id="" className="l-header__select"></select>
+          </div>
+
+          <div className="l-header__infos">
+            <div className="l-header__info">
+              <p className="l-header__clap">拍手できる数 </p>
+              <p>100</p>
+              <p>{this.props.user_info}</p>
             </div>
 
-            <div className="l-header__infos">
-                <div className="l-header__info">
-                    <p className="l-header__clap">拍手できる数 </p>
-                    <p>100</p>
-                </div>
-
-                <div className="l-header__info">
-                    <p className="l-header__clap">拍手された数 </p>
-                    <p>0</p>
-                </div>
+            <div className="l-header__info">
+              <p className="l-header__clap">拍手された数 </p>
+              <p>0</p>
             </div>
+          </div>
         </div>
-
-    </div>
-      );
+      </div>
+    );
   }
 }
 
